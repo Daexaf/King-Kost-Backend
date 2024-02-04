a498c2ff9c8fce8ad72faede257b4039b9aeb313
USER <andreriyantoo19@gmail.com>
2024-02-05 00:00:06 +0700

    initial commit

---
+++ src/main/java/com/enigma/kingkost/KingKostApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.kingkost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingKostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingKostApplication.class, args);
+	}
+
+}
